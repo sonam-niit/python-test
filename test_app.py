d85a52a7b822b40d6295a0db76ff4f24d5853bdd
sonam-niit <sonam.gravity@gmail.com>
2025-05-24 12:03:41 +0530

    subtraction test added

--- test_app.py
+++ test_app.py
@@ -1,7 +1,12 @@
 import pytest
-from app import add_numbers
+from app import add_numbers,sub_numbers
 
 def test_add_numbers():
     assert add_numbers(2,3)==5
     assert add_numbers(-1,1)==0
     assert add_numbers(0,0)==0
+    
+def test_sub_numbers():
+    assert sub_numbers(2,3)==-1
+    assert sub_numbers(-1,1)==-2
+    assert sub_numbers(0,0)==0
